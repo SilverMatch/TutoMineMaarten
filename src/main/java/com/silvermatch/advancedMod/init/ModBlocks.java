71c23d969bba36d39ba4b3485b2fc44c42465c86
SilverMatch_Laptop <sebastien.neveu2@gmail.com>
2015-02-16 21:22:46 +0100

    Fix #1 :)

--- src/main/java/com/silvermatch/advancedMod/init/ModBlocks.java
+++ src/main/java/com/silvermatch/advancedMod/init/ModBlocks.java
@@ -1,6 +1,7 @@
 package com.silvermatch.advancedMod.init;
 
 import com.silvermatch.advancedMod.reference.Reference;
+import com.silvermatch.advancedMod.utility.LogHelper;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
@@ -8,5 +9,6 @@ public class ModBlocks {
 
 	public static void init(){
 		//test
+		LogHelper.info("Init Blocks Complete!");
 	}
 }
